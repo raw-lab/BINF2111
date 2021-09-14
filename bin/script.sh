9472368bb36489b8c5e963e401bb24e1e1239516
RAW Lab <73349211+raw-lab@users.noreply.github.com>
2021-09-13 23:09:01 -0400

    Add files via upload

---
+++ bin/script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Echo this is my first
+echo "Hello world! This is my first Bash script!"
+echo -n "I am executing he script with user: "
+whoami
+echo -n "I am currently running in the directory: "
+pwd
+exit 0
