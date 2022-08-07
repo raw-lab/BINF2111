53c2304fbba5efac4b522da8e72db7d0c01debe0
RAW Lab <73349211+raw-lab@users.noreply.github.com>
2022-08-07 13:56:59 -0400

    Delete GCCount.py

--- bin/GCCount.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-my_dna = 'ACTGATCCATATATATTTT' 
-length = len(my_dna)
-g_count = my_dna.count('G')
-c_count = my_dna.count('C')
-gc_content = (g_count + c_count) / (length)
-print('Hello...')
-print('GC content is: ' + str(gc_content))
